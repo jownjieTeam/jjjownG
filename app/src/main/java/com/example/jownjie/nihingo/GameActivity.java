1b3b2400184e8f26e104d87d68fb0a8e744270df
Prince Niko Garces <prince.nikogarces@gmail.com>
2015-12-08 23:11:58 +0800

    enhanced ui for activity_game.xml, added functionality to buttons

--- app/src/main/java/com/example/jownjie/nihingo/GameActivity.java
+++ app/src/main/java/com/example/jownjie/nihingo/GameActivity.java
@@ -1,11 +1,12 @@
 package com.example.jownjie.nihingo;
 
 import android.os.Bundle;
-import android.support.annotation.RawRes;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.jownjie.nihingo.Database.DatabaseConnector;
@@ -21,12 +22,17 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+    private String answer="";
+    private Button[] buttons;
+    private int wordCount=0;
+    private String newAnswer;
     DatabaseConnector dc;
     GamePool temp;
 
-    @Bind(R.id.textView)
-    TextView textView;
+    @Bind(R.id.ans_container)
+    LinearLayout ansContainer;
+
 
     @Bind(R.id.imageView)
     ImageView imageView;
@@ -37,10 +43,38 @@ public class GameActivity extends AppCompatActivity {
             R.id.button4,
             R.id.button5,
             R.id.button6,
-            R.id.button7})
+            R.id.button7,
+            R.id.button8,
+            R.id.button9,
+            R.id.button10})
     public void displayCharacters(Button button){
-        textView.append(button.getText().toString());
-        //textView.setText(button.getText().toString());
+        button.setVisibility(View.INVISIBLE);
+        appendToContainer(button.getText().toString());
+    }
+
+    public void initAnswerContainer(int count){
+        for(int i = 0; i < newAnswer.length(); i++){
+            LinearLayout.LayoutParams btnparams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            Button btn = new Button(this, null, android.R.attr.buttonStyleSmall);
+            btn.setLayoutParams(btnparams);
+            btn.setOnClickListener(this);
+            ansContainer.addView(btn);
+
+            buttons[i] = btn;
+        }
+    }
+
+    public void appendToContainer(String letter){
+        answer+=letter;
+        for(int i =0;i<buttons.length;i++){
+            if(buttons[i].getText().equals("")){
+                buttons[i].setText(letter);
+                break;
+            }
+        }
+        if(answer.equals(newAnswer)){
+            Toast.makeText(this, "congratulations dickface no one cares", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -48,16 +82,26 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         ButterKnife.bind(this);
+
         dc = new DatabaseConnector(this,1);
 
-        if(dc.insertGameLevel(new GamePool(null,"TEST","TESTHINT"),createFileFromResource(R.drawable.advanced_avocado,"advanced_avocado.png")))
+        /***/
+        newAnswer = "COCK";
+        int newDr = R.drawable.advanced_coconut;
+        String fileName = "advanced_coconut.png";
+        /****/
+
+        buttons = new Button[newAnswer.length()];
+        initAnswerContainer(buttons.length);
+
+        if(dc.insertGameLevel(new GamePool(null,newAnswer,"TESTHINT"),createFileFromResource(newDr,fileName)))
             Toast.makeText(this,"SUCCESS!", Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(this,"FAILED!", Toast.LENGTH_SHORT).show();
-        temp = dc.getGameLevel("TEST");
+        temp = dc.getGameLevel(newAnswer);
         if(temp!=null) {
             imageView.setImageBitmap(temp.getImageDr());
-            Toast.makeText(this,String.valueOf(temp.getAnswer()),Toast.LENGTH_LONG);
+            Toast.makeText(this, String.valueOf(temp.getAnswer()), Toast.LENGTH_LONG);
         }
     }
 
@@ -85,5 +129,11 @@ public class GameActivity extends AppCompatActivity {
             f = null;
         }
         return f;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button)v;
+        button.setText("");
     }
 }
