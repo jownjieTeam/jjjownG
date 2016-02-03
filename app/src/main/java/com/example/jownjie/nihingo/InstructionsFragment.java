9e7a45273acb6c9cdb0dadb2accdefca2ea55e32
Prince Niko Garces <Prince Niko Garces>
2016-02-03 22:57:39 +0800

    scandal ni bro

--- app/src/main/java/com/example/jownjie/nihingo/InstructionsFragment.java
+++ app/src/main/java/com/example/jownjie/nihingo/InstructionsFragment.java
@@ -1,13 +1,20 @@
 package com.example.jownjie.nihingo;
 
 import android.app.Fragment;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -15,6 +22,9 @@ import butterknife.OnClick;
  * Created by hebi5 on 1/24/2016.
  */
 public class InstructionsFragment extends Fragment {
+    @Bind(R.id.instructions_msg_image)
+    ImageView imageView;
+
     View rootView;
 
     @OnClick(R.id.button_back_menu)
@@ -28,6 +38,47 @@ public class InstructionsFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_instructions, container, false);
         ButterKnife.bind(this, rootView);
 
+        imageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(), R.drawable.intructions_msg, ViewGroup.LayoutParams.WRAP_CONTENT, 500));
+        Log.e("a", "with the ultimate code");
+
         return rootView;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
     }
 }
